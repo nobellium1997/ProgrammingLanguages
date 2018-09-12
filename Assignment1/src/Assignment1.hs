8753d904b238d4b97d33491c8d385c597aaf3b8c
nobel <nobarakat@gmail.com>
2018-09-12 17:11:20 -0600

    finished part 3

--- Assignment1/src/Assignment1.hs
+++ Assignment1/src/Assignment1.hs
@@ -1,6 +1,8 @@
 module Assignment1
     ( sDotProduct,
-    distance
+    distance,
+    tripleDistance
     ) where
 sDotProduct  x (arg1, arg2) (arg3, arg4) = ((arg1 * arg2) + (arg3 * arg4)) * x
 distance (x1, y1) (x2, y2)  = sqrt ((x2 - x1) ^ 2  + (y2 - y1) ^ 2)
+tripleDistance (x1, y1, z1) (x2, y2, z2) = sqrt ((x2-x1) ^ 2 + (y2 - y1) ^ 2 + (z2 - z1) ^ 2)
