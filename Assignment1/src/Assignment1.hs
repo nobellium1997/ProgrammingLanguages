3da89e214f47e32874dd5599190aebabca0d2ce2
nobel <nobarakat@gmail.com>
2018-09-12 16:55:45 -0600

    completed part 2

--- Assignment1/src/Assignment1.hs
+++ Assignment1/src/Assignment1.hs
@@ -1,4 +1,6 @@
 module Assignment1
-    ( sDotProduct
+    ( sDotProduct,
+    distance
     ) where
 sDotProduct  x (arg1, arg2) (arg3, arg4) = ((arg1 * arg2) + (arg3 * arg4)) * x
+distance (x1, y1) (x2, y2)  = sqrt ((x2 - x1) ^ 2  + (y2 - y1) ^ 2)
