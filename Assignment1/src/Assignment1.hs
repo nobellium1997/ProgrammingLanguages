2a0f126696879eb045b1fb544b5de3f184d5ed19
nobel <nobarakat@gmail.com>
2018-09-12 18:44:16 -0600

    completed findMin

--- Assignment1/src/Assignment1.hs
+++ Assignment1/src/Assignment1.hs
@@ -1,8 +1,12 @@
 module Assignment1
     ( sDotProduct,
     distance,
-    tripleDistance
+    tripleDistance,
+    findMin
     ) where
 sDotProduct  x (arg1, arg2) (arg3, arg4) = ((arg1 * arg2) + (arg3 * arg4)) * x
 distance (x1, y1) (x2, y2)  = sqrt ((x2 - x1) ^ 2  + (y2 - y1) ^ 2)
 tripleDistance (x1, y1, z1) (x2, y2, z2) = sqrt ((x2-x1) ^ 2 + (y2 - y1) ^ 2 + (z2 - z1) ^ 2)
+-- using the maximum example from "learn you a haskell textbook
+findMin [x] = x
+findMin (x:y:xs) = if (x < y) then findMin(x:xs) else findMin(y:xs)
