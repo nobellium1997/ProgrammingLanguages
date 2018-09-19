09ea9854b055ca96f63d5658e84ce077be076d78
nobel <nobel@debian>
2018-09-19 17:25:02 -0600

    completed substitute and began mergesort

--- Assignment2/src/Assignment2.hs
+++ Assignment2/src/Assignment2.hs
@@ -1,6 +1,8 @@
 module Assignment2
     ( removeAllExcept,
-      removeAll
+      removeAll,
+      substitute,
+      mergeSorted3
     ) where
 
 -- Remove All except
@@ -12,4 +14,12 @@ removeAllExcept a (x:xs) = if (a /= x) then removeAllExcept a xs else x : remove
 removeAll :: Eq a => a -> [a] -> [a]
 removeAll a [] = []
 removeAll a (x:xs) = if (a == x) then removeAll a xs else x : removeAll a xs
+
+-- substitute
+substitute :: Eq a => a -> a -> [a] -> [a]
+substitute a b [] = []
+substitute a b (x:xs) = if (a == x) then b : substitute a b xs else x : substitute a b xs
+
+-- mergeSorted3
+mergeSorted3 :: Ord a => [a] -> [a] -> [a] -> [a]
 
