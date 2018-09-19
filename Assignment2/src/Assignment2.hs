74af20aeefe33a102005018afceb2451bb933ddf
nobel <nobel@debian>
2018-09-19 17:07:15 -0600

    completed part 2

--- Assignment2/src/Assignment2.hs
+++ Assignment2/src/Assignment2.hs
@@ -1,7 +1,15 @@
 module Assignment2
-    ( removeAllExcept
+    ( removeAllExcept,
+      removeAll
     ) where
 
+-- Remove All except
 removeAllExcept :: Eq a => a -> [a] -> [a]
 removeAllExcept a [] = []
-removeAllExcept a (x:xs) = if (a == x) then removeAllExcept a xs else x : removeAllExcept a xs
+removeAllExcept a (x:xs) = if (a /= x) then removeAllExcept a xs else x : removeAllExcept a xs
+
+-- Remove All
+removeAll :: Eq a => a -> [a] -> [a]
+removeAll a [] = []
+removeAll a (x:xs) = if (a == x) then removeAll a xs else x : removeAll a xs
+
