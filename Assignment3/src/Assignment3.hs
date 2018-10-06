cc3f929808d5cbbeb8f6013aa00a5dbecf73dac9
nobel <nobarakat@gmail.com>
2018-10-06 17:45:39 -0600

    added onlyLowercase

--- Assignment3/src/Assignment3.hs
+++ Assignment3/src/Assignment3.hs
@@ -1,3 +1,9 @@
 module A3b where
 
+import Data.Char(isLower)
+import Data.List(filter)
+
+onlyLowercase :: [String] -> [String]
+onlyLowercase xs = filter isLowerHelper xs
+  where isLowerHelper xs = isLower (head xs)
 
