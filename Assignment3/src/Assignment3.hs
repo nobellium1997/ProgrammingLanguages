3c738e7828be47d969246a4b6c969413d20fff70
nobel <nobarakat@gmail.com>
2018-10-07 13:46:22 -0600

    completed longestString 1

--- Assignment3/src/Assignment3.hs
+++ Assignment3/src/Assignment3.hs
@@ -3,7 +3,12 @@ module A3b where
 import Data.Char(isLower)
 import Data.List(filter)
 
+-- onlyLowercase
 onlyLowercase :: [String] -> [String]
 onlyLowercase xs = filter isLowerHelper xs
   where isLowerHelper xs = isLower (head xs)
 
+-- longestString (first version)
+longestString :: [String] -> String
+longestString [] = ""
+longestString xs = foldl (\x y -> if x > y then x else y) "" xs
