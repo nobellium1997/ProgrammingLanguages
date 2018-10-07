7c8e9fa45c4663318d132195e86952eefe13824c
nobel <nobarakat@gmail.com>
2018-10-07 15:28:08 -0600

    in progress helper

--- Assignment3/src/Assignment3.hs
+++ Assignment3/src/Assignment3.hs
@@ -15,4 +15,27 @@ longestString xs = foldl (\x y -> if length x >= length y then x else y) "" xs
 
 -- longestString (second version)
 longestString' :: [String] -> String
+longestString' [] = ""
 longestString' xs = foldl (\x y -> if length x > length y then x else y) "" xs
+
+-- longestStringHelper
+longestStringHelper :: (Int -> Int -> Bool) -> [String] -> String
+longestStringHelper _ [] = ""
+longestStringHelper f xs = longestStringHelper' f (head xs) xs
+  where
+  longestStringHelper' f acc [x] = if f (length acc) (length x)
+                                      then acc
+                                      else x
+  longestStringHelper' f acc (x:xs) = if (f (length acc) (length x))
+                                        then longestStringHelper' f acc xs
+                                        else longestStringHelper' f x xs
+
+-- longestString3
+longestString3 :: [String] -> String
+longestString3 xs = longestStringHelper (\x y -> x >= y) xs
+
+-- longestString4
+longestString4 :: [String] -> String
+longestString4 xs = longestStringHelper (\x y -> x > y) xs
+
+
