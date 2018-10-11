25c93b6fc5733c3e6b2e7033a59b71ac168012a4
nobel <nobarakat@gmail.com>
2018-10-10 21:51:40 -0600

    completed up to checkPat

--- Assignment3/src/Assignment3.hs
+++ Assignment3/src/Assignment3.hs
@@ -7,27 +7,42 @@ import Data.Maybe
 -- onlyLowercase
 onlyLowercase :: [String] -> [String]
 onlyLowercase xs = filter isLowerHelper xs
-  where isLowerHelper xs = isLower (head xs)
+  where
+    isLowerHelper xs = isLower (head xs)
 
 -- longestString (first version)
 longestString :: [String] -> String
 longestString [] = ""
-longestString xs = foldl (\x y -> if length x >= length y then x else y) "" xs
+longestString xs =
+  foldl
+    (\x y ->
+       if length x >= length y
+         then x
+         else y)
+    ""
+    xs
 
 -- longestString (second version)
 longestString' :: [String] -> String
 longestString' [] = ""
-longestString' xs = foldl (\x y -> if length x > length y then x else y) "" xs
+longestString' xs =
+  foldl
+    (\x y ->
+       if length x > length y
+         then x
+         else y)
+    ""
+    xs
 
 -- longestStringHelper
 longestStringHelper :: (Int -> Int -> Bool) -> [String] -> String
 longestStringHelper _ [] = ""
 longestStringHelper f xs = foldl f' "" xs
   where
-  f' x y = if f (length x) (length y) == True
-             then x
-             else y
-
+    f' x y =
+      if f (length x) (length y) == True
+        then x
+        else y
 
 -- longestString3
 longestString3 :: [String] -> String
@@ -45,21 +60,89 @@ longestLowercase xs = (longestString . onlyLowercase) xs
 revStringRev :: String -> String
 revStringRev x = (lowerString . reverse) x
   where
-  lowerString [x] = [toLower x]
-  lowerString (x:xs) = toLower x : lowerString xs
+    lowerString [x] = [toLower x]
+    lowerString (x:xs) = toLower x : lowerString xs
 
 -- firstAnswer
-firstAnswer :: ( a -> Maybe b ) -> [a] -> Maybe b
+firstAnswer :: (a -> Maybe b) -> [a] -> Maybe b
 firstAnswer _ [] = Nothing
-firstAnswer f (x:xs) = if isNothing (f x) then firstAnswer f xs else f x
+firstAnswer f (x:xs) =
+  if isNothing (f x)
+    then firstAnswer f xs
+    else f x
 
 -- allAnswers
-allAnswers :: (a -> Maybe [b]) -> [a] -> Maybe [b]
+allAnswers :: (a -> Maybe [b]) -> [a] -> Maybe [b]
 allAnswers _ [] = Nothing
 allAnswers f (x:xs) = allAnswersHelper f (f x) xs
   where
-  allAnswersHelper f acc [] = acc
-  allAnswersHelper f acc (x:xs) = if isNothing (f x)
-                                    then Nothing
-                                    else allAnswersHelper f (concatinate acc (f x)) xs
-  concatinate (Just x) (Just y) = Just (x ++ y)
+    allAnswersHelper f acc [] = acc
+    allAnswersHelper f acc (x:xs) =
+      if isNothing (f x)
+        then Nothing
+        else allAnswersHelper f (cat acc (f x)) xs
+    cat (Just x) (Just y) = Just (x ++ y)
+
+data Pattern
+  = WildcardPat
+  | VariablePat (String)
+  | UnitPat
+  | ConstantPat (Int)
+  | ConstructorPat (String, Pattern)
+  | TuplePat ([Pattern])
+  deriving (Eq, Show)
+
+data Value
+  = Constant (Int)
+  | Unit
+  | Constructor (String, Value)
+  | Tuple [Value]
+  deriving (Eq, Show)
+
+g f1 f2 pat =
+  let r = g f1 f2
+   in case pat of
+        WildcardPat -> f1 ()
+        VariablePat x -> f2 x
+        ConstructorPat (_, p) -> r p
+        TuplePat values -> foldl (\i p -> (r p) + i) 0 values
+        _ -> 0
+
+-- 1
+-- Sub problem 1: The arguments g takes is two functions and a pattern.
+--                g has to return a number (the type of number depends on the functions being passed)
+-- Sub problem 2:
+countWildcards :: Pattern -> Int
+countWildcards pat = g (\x -> 1) (\y -> 0) pat
+
+-- Sub problem 3:
+countWildAndVariableLengths :: Pattern -> Int
+countWildAndVariableLengths pat = g (\x -> 1) (\y -> (length y)) pat
+
+-- Sub problem 4:
+countAVar :: (String, Pattern) -> Int
+countAVar (str, pat) =
+  g (\x -> 0)
+    (\y ->
+       if y == str
+         then 1
+         else 0)
+    pat
+
+-- checkPat
+checkPat :: Pattern -> Bool
+checkPat pat = checkList (makeList pat)
+  where
+    makeList (VariablePat x) = [x]
+    makeList (ConstantPat x) = [show x]
+    makeList (ConstructorPat (_, p)) = makeList p
+    makeList (TuplePat values) = foldl (\x y -> (makeList y) ++ x) [] values
+    checkList [] = True
+    checkList (x:xs) =
+      if elem x xs
+        then False
+        else checkList xs
+
+-- match
+--match :: (Value, Pattern) -> Maybe [(String, Value)]
+--match (val, pat) =
